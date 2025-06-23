fd74517aa045b6b19c821db87eed696b1f3c46c4
Niran Wangyad <niran.w@cicnetgrp.net>
2025-06-23 17:19:41 +0700

    update

--- backend/controllers/JournaltransController.php
+++ backend/controllers/JournaltransController.php
@@ -43,7 +43,7 @@ class JournaltransController extends Controller
     public function actionIndex()
     {
         $pageSize = \Yii::$app->request->post("perpage");
-        $searchModel = new JournalTransSearch();
+        $searchModel = new \backend\models\journalTransSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         $dataProvider->setSort(['defaultOrder' => ['id' => SORT_ASC]]);
         $dataProvider->pagination->pageSize = $pageSize;
