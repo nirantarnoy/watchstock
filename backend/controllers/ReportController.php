b78178d5adacfc84dd84928011f8bf4fa91f8d74
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-01 19:28:07 +0700

    update

--- backend/controllers/ReportController.php
+++ backend/controllers/ReportController.php
@@ -24,24 +24,24 @@ class ReportController extends Controller
 //                    'delete' => ['POST','GET'],
 //                ],
 //            ],
-            'access'=>[
-                'class'=>AccessControl::className(),
-                'denyCallback' => function ($rule, $action) {
-                    throw new ForbiddenHttpException('คุณไม่ได้รับอนุญาติให้เข้าใช้งาน!');
-                },
-                'rules'=>[
-                    [
-                        'allow'=>true,
-                        'roles'=>['@'],
-                        'matchCallback'=>function($rule,$action){
-                            $currentRoute = \Yii::$app->controller->getRoute();
-                            if(\Yii::$app->user->can($currentRoute)){
-                                return true;
-                            }
-                        }
-                    ]
-                ]
-            ],
+//            'access'=>[
+//                'class'=>AccessControl::className(),
+//                'denyCallback' => function ($rule, $action) {
+//                    throw new ForbiddenHttpException('คุณไม่ได้รับอนุญาติให้เข้าใช้งาน!');
+//                },
+//                'rules'=>[
+//                    [
+//                        'allow'=>true,
+//                        'roles'=>['@'],
+//                        'matchCallback'=>function($rule,$action){
+//                            $currentRoute = \Yii::$app->controller->getRoute();
+//                            if(\Yii::$app->user->can($currentRoute)){
+//                                return true;
+//                            }
+//                        }
+//                    ]
+//                ]
+//            ],
         ];
     }
     public function actionIndex()
