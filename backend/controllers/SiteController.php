983fb8a28796da28c680ec8f5ef3954dc8debe91
Niran Wangyad <niran.w@cicnetgrp.net>
2025-08-07 19:57:23 +0700

    update

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -336,7 +336,7 @@ class SiteController extends Controller
                 'SUM(jtl.qty * jtl.sale_price) as total_sales',
                 'AVG(jtl.sale_price) as avg_price',
                 'AVG(p.cost_price) as cost_price',
-                'SUM(jtl.qty * jtl.cost_price) - SUM(jtl.qty * p.cost_price) as profit'
+                'SUM(jtl.qty * p.sale_price) - SUM(jtl.qty * p.cost_price) as profit'
             ])
             ->from(['jtl' => 'journal_trans_line'])
             ->innerJoin(['p' => 'product'], 'jtl.product_id = p.id')
