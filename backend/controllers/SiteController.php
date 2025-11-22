d3170bd894e6ce6bccbc0f171f5bf703ff7de954
Niran Wangyad <niran.w@cicnetgrp.net>
2025-11-22 11:08:26 +0700

    update

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -392,7 +392,9 @@ class SiteController extends Controller
         $query = (new Query())
             ->select([
                 'pb.name',
-               // 'SUM(p.cost_price) as cost_price',
+              //  'SUM(p.cost_price) as cost_price',
+                'SUM(jtl.sale_price * jtl.qty) as avg_sale_price',
+                'SUM(jtl.qty) as total_qty',
                 new \yii\db\Expression("
                     CASE 
                         WHEN COALESCE(AVG(jtl.line_price), 0) = 0 
@@ -400,8 +402,11 @@ class SiteController extends Controller
                         ELSE AVG(jtl.line_price) 
                     END AS cost_price
                 "),
-                'SUM(jtl.sale_price * jtl.qty) as avg_sale_price',
-                'SUM(jtl.qty) as total_qty'
+                new \yii\db\Expression("
+                    SUM(jtl.qty * jtl.sale_price) - 
+                    SUM(jtl.qty * COALESCE(NULLIF(jtl.line_price, 0), p.cost_price)) 
+                    AS profit
+                ")
             ])
             ->from(['jtl' => 'journal_trans_line'])
             ->innerJoin(['p' => 'product'], 'jtl.product_id = p.id')
