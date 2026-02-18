01d3b939afce8ecadd7d4984c59d7116e3a88cd7
Niran Wangyad <niran.w@cicnetgrp.net>
2026-02-18 13:05:31 +0700

    update

--- backend/controllers/DbbackupController.php
+++ backend/controllers/DbbackupController.php
@@ -60,17 +60,24 @@ class DbbackupController extends Controller
 
     public function actionBak()
     {
-        $host = "localhost";
-        $username = "root";
-        $password = "";
-        $database_name = "coltd";
+        $db = Yii::$app->db;
+        $dsn = $db->dsn;
+        
+        preg_match('/host=([^;]+)/', $dsn, $matches_host);
+        preg_match('/dbname=([^;]+)/', $dsn, $matches_db);
+        
+        $host = $matches_host[1] ?? 'localhost';
+        $database_name = $matches_db[1] ?? '';
+        $username = $db->username;
+        $password = $db->password;
 
-// Get connection object and set the charset
         $conn = mysqli_connect($host, $username, $password, $database_name);
+        if (!$conn) {
+             Yii::$app->session->setFlash('error', 'Could not connect to database.');
+             return $this->redirect(['backuplist']);
+        }
         $conn->set_charset("utf8");
 
-
-// Get All Table Names From the Database
         $tables = array();
         $sql = "SHOW TABLES";
         $result = mysqli_query($conn, $sql);
@@ -81,51 +88,40 @@ class DbbackupController extends Controller
 
         $sqlScript = "";
         foreach ($tables as $table) {
-
-            // Prepare SQLscript for creating table structure
             $query = "SHOW CREATE TABLE $table";
             $result = mysqli_query($conn, $query);
             $row = mysqli_fetch_row($result);
 
             $sqlScript .= "\n\n" . $row[1] . ";\n\n";
 
-
             $query = "SELECT * FROM $table";
             $result = mysqli_query($conn, $query);
-
             $columnCount = mysqli_num_fields($result);
 
-            // Prepare SQLscript for dumping data for each table
-            for ($i = 0; $i < $columnCount; $i++) {
-                while ($row = mysqli_fetch_row($result)) {
-                    $sqlScript .= "INSERT INTO $table VALUES(";
-                    for ($j = 0; $j < $columnCount; $j++) {
-                        $row[$j] = $row[$j];
-
-                        if (isset($row[$j])) {
-                            $sqlScript .= '"' . $row[$j] . '"';
-                        } else {
-                            $sqlScript .= '""';
-                        }
-                        if ($j < ($columnCount - 1)) {
-                            $sqlScript .= ',';
-                        }
+            while ($row = mysqli_fetch_row($result)) {
+                $sqlScript .= "INSERT INTO $table VALUES(";
+                for ($j = 0; $j < $columnCount; $j++) {
+                    if (isset($row[$j])) {
+                        $row[$j] = mysqli_real_escape_string($conn, $row[$j]);
+                        $sqlScript .= '"' . $row[$j] . '"';
+                    } else {
+                        $sqlScript .= 'NULL';
                     }
-                    $sqlScript .= ");\n";
+                    if ($j < ($columnCount - 1)) {
+                        $sqlScript .= ',';
+                    }
                 }
+                $sqlScript .= ");\n";
             }
-
             $sqlScript .= "\n";
         }
 
         if (!empty($sqlScript)) {
-            // Save the SQL script to a backup file
             $backup_file_name = $database_name . '_backup_' . time() . '.sql';
             $fileHandler = fopen($backup_file_name, 'w+');
-            $number_of_lines = fwrite($fileHandler, $sqlScript);
+            fwrite($fileHandler, $sqlScript);
             fclose($fileHandler);
 
-            // Download the SQL backup file to the browser
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename=' . basename($backup_file_name));
@@ -137,7 +133,8 @@ class DbbackupController extends Controller
             ob_clean();
             flush();
             readfile($backup_file_name);
-            exec('rm ' . $backup_file_name);
+            unlink($backup_file_name);
+            exit();
         }
 
 
@@ -229,30 +226,31 @@ class DbbackupController extends Controller
 
     public function actionExrestore()
     {
-        $host = "localhost";
-        $username = "root";
-        $password = "";
-      //  $database_name = "admin_icesystem";
-        $database_name = "mmc_db";
+        $db = Yii::$app->db;
+        $dsn = $db->dsn;
+        
+        preg_match('/host=([^;]+)/', $dsn, $matches_host);
+        preg_match('/dbname=([^;]+)/', $dsn, $matches_db);
+        
+        $host = $matches_host[1] ?? 'localhost';
+        $database_name = $matches_db[1] ?? '';
+        $username = $db->username;
+        $password = $db->password;
+        
         $date_string = time();
 
         $cmd = '';
 
         $os = php_uname();
         if (strpos($os, 'ndow') > 0) {
-            $cmd = 'D:/xampp/mysql/bin/';
-            $cmd .= "mysqldump -h {$host} -u {$username} {$database_name} > " . '../web/uploads/backup/' . "pc_{$date_string}_{$database_name}.sql";
-
+            $cmd = 'mysqldump -h ' . $host . ' -u ' . $username . ($password ? ' -p' . $password : '') . ' ' . $database_name . ' > ' . '../web/uploads/backup/' . 'pc_' . $date_string . '_' . $database_name . '.sql';
         } else {
-            //    $cmd ='/usr/bin/';
-            $cmd = "/usr/bin/mysqldump -u {$username} -p{$password} {$database_name} > " . '../web/uploads/backup/' . "web_{$date_string}_{$database_name}.sql";
+            $cmd = "mysqldump -h {$host} -u {$username} -p'{$password}' {$database_name} > " . '../web/uploads/backup/' . "web_{$date_string}_{$database_name}.sql";
         }
-
 
         exec($cmd);
 
         return $this->redirect(['dbbackup/backuplist']);
-
     }
 
     public function backup_tables($host, $user, $pass, $name, $tables = '*')
